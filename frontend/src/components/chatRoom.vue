18386c363c55d753dfdb0a97562afd7af27aa8b2
MJ <pingukim@naver.com>
2020-02-22 13:43:54 +0900

    공통 css 적용

---
+++ frontend/src/components/chatRoom.vue
@@ -0,0 +1,56 @@
+<template>
+    <body class="body-chat">
+    <header class= "top-header chat-header">
+        <div class="header__top">
+            <div class="header__column">
+                <i class="fas fa-plane"></i>
+                <i class="fas fa-wifi"></i>
+            </div>
+        </div>
+        <div class="header__bottom">
+            <div class="header__column">
+                <a href="chats.html">
+                    <i class="fa fa-chevron-left fa-lg"></i>
+                </a>
+            </div>
+            <div class="header__column">
+                <span class="header__text">유정</span>
+            </div>
+            <div class="header__column">
+                <i class="fa fa-search"></i>
+                <i class="fa fa-bars"></i>
+            </div>
+        </div>
+    </header>
+    <main class="chat">
+        <div class="date-divider">
+            <span class="date-divider__text">화요일, 2018년 6월 19일</span>
+        </div>
+        <div class="chat__message chat__message-from-me">
+            <span class="chat__message-time">17:55</span>
+            <span class="chat__message-body">유정아 밥 먹었어?</span>
+        </div>
+
+        <div class="chat__message chat__message-to-me">
+            <img src="images/kakao_friends.png" class="chat__message-avaatar">
+            <div class="chat__message-center">
+                <h3 class="chat__message-username">유정</h3>
+                <span class="chat__message-body">응 파스타 먹었어!! 건상아 넌 뭐하고있었어?</span>
+            </div>
+
+            <span class="chat__message-time">17:57</span>
+        </div>
+
+    </main>
+    <div class="type-message">
+        <i class="fa fa-plus "></i>
+        <div class="type-message__input">
+            <input type="text">
+            <i class="fa fa-smile-o"></i>
+            <span class="record-message">
+        <i class="fa fa-microphone"></i>
+      </span>
+        </div>
+    </div>
+    </body>
+</template>
