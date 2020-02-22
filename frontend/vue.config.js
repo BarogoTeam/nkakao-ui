aca76b112242bd43015da2dee6837d82ab3bc92f
MJ <pingukim@naver.com>
2020-02-22 15:49:17 +0900

    파일들 원복

--- frontend/vue.config.js
+++ frontend/vue.config.js
@@ -1,6 +1,14 @@
 module.exports = {
     devServer: {
-proxy: {
-},
-outputDir: '../backend/public',
+        proxy: {
+            '/v1': {
+                target: 'http://localhost:3000/v1',
+                changeOrigin: true,
+                pathRewrite: {
+                    '^/v1': ''
+                }
+            }
+        }
+    },
+    outputDir: '../backend/public',
 }
