7dfbb9007da7808f13a75524561150cfad58033b
heejun <vsv21gml@naver.com>
2020-05-09 13:22:03 +0900

    메인화면 아이콘 추가 (add font-awesome cdn)

--- frontend/src/components/main.vue
+++ frontend/src/components/main.vue
@@ -2,12 +2,17 @@
     <div class="app">
         <div class="sidebar">
             <div v-for="side of sideList" :key="side.id" class="sidebar-item" :style="side.style">
-                <i class="sidebar-item-icon"/>
+                <i :class="side.class" class="sidebar-item-icon"/>
                 <div class="sidebar-item-text">{{side.title}}</div>
             </div>
         </div>
         <div class="main">
             <div class="menubar">
+                <div class="menu-tool-list">
+                    <i class="menu-tool-item fa fa-times"/>
+                    <i class="menu-tool-item fa fa-window-maximize"/>
+                    <i class="menu-tool-item fa fa-window-minimize"/>
+                </div>
             </div>
             <div class="content">
             </div>
@@ -18,9 +23,9 @@
     export default {
         data: () => ({
             sideList: [
-                {id: 'user', title: 'user', style: {background:'#AAA'}},
-                {id: 'chat', title: 'chatting', style: {background:'#FFF'}},
-                {id: 'setting', title: 'setting', style: {background:'#AFA'}},
+                {id: 'user', title: 'user', class: 'fa fa-user', style: {background:'#AAA'}},
+                {id: 'chat', title: 'chatting', class: 'fa fa-comment', style: {background:'#FFF'}},
+                {id: 'setting', title: 'setting', class: 'fa fa-search', style: {background:'#AFA'}},
             ]
         })
     }
